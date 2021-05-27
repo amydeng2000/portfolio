2149d0a0c1fd8d374438098215d7e6c78873a81b
Amy Personal <amy_personal@adeng.local>
2021-05-27 11:33:26 -0400

    finished homepage

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React from 'react'
+import image from '../bgimg.png'
 
 export default function Home() {
-    return <h1> Home Page </h1>
+  return (
+    <main>
+      <img
+        src={image}
+        alt="background img"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative justify-center flex-row min-h-screen pt-12 lg:pt-64 px-8 ">
+        <h1 className="text-center text-6xl text-white font-bold cursive leading-none lg: leadning-snug home-name">
+          Hey there!
+        </h1>
+        <h1 className="text-center text-6xl text-white font-bold cursive leading-none lg: leadning-snug home-name">
+          I'm Amy
+        </h1>
+      </section>
+    </main>
+  )
 }
--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,5 +1,69 @@
-import React from "react"
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { SocialIcon } from 'react-social-icons'
 
 export default function NavBar() {
-    return <h1> NavBar Page </h1>
+  return (
+    <header className="bg-indigo-500">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-pink-300"
+            className="inline-flex items-center py-6 px-3 mr-4 text-pink-100 hover:text-pink-300 text-4xl font-bold cursive tracking-widset"
+          >
+            Amy
+          </NavLink>
+          <NavLink
+            to="/post"
+            activeClassName="text-white bg-indigo-500"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-pink-100 hover:text-white"
+          >
+            Writings
+          </NavLink>
+          <NavLink
+            to="/project"
+            activeClassName="text-white bg-indigo-500"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-pink-100 hover:text-white"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            activeClassName="text-white bg-indigo-500"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-pink-100 hover:text-white"
+          >
+            About
+          </NavLink>
+        </nav>
+        <div className="inline-flex px-3 py-3 my-6">
+          <SocialIcon
+            url="https://twitter.com/amydeng_"
+            className="ml-6"
+            target="_blank"
+            fgColor="#fff"
+            bgColor="#F9A8D4"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/amykexindeng/"
+            className="ml-6"
+            target="_blank"
+            fgColor="#fff"
+            bgColor="#F9A8D4"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.facebook.com/amy.deng.16"
+            className="ml-6"
+            target="_blank"
+            fgColor="#fff"
+            bgColor="#F9A8D4"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  )
 }
