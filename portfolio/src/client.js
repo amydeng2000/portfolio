dbf728b2b94e2498910b9ae00d28586cb69990d4
Amy Personal <amy_personal@adeng.local>
2021-06-07 11:05:50 -0400

    made sanity studio work

--- portfolio/src/client.js
+++ portfolio/src/client.js
@@ -1,6 +1,6 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-    projectID: "t6ywgvw0",
-    dataset: "production"
+  projectId: 't6ywgvw0',
+  dataset: 'production',
 })
--- portfolio/src/components/Post.js
+++ portfolio/src/components/Post.js
@@ -1,5 +1,62 @@
-import React from "react"
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import sanityClient from '../client.js'
 
 export default function Post() {
-    return <h1> Post Page </h1>
+  const [postData, setPost] = useState(null)
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"] {
+        title,
+        slug,
+        mainImage{
+            asset->{
+                _id,
+                url
+            },
+            alt
+        }
+    }`,
+      )
+      .then((data) => setPost(data))
+      .catch(console.error)
+  }, [])
+  console.log(postData)
+  return (
+    <main className="bg-blue-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive mb-2">
+          Journals &amp; Blogs
+        </h1>
+        <h2 className="text-lg text-grey-600 flex justify-center mb-12">
+          Welcome to my stream of consciousness!
+        </h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={'/post/' + post.slug.current} key={post.slug.current}>
+                  <span
+                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-pink-200 hover:border-pink-300"
+                    key={index}
+                  >
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                      className="w-full h-full rounded-r object-cover absolute filter brightness-75"
+                    />
+                    <span className="block relative h-full flex justify-center items-center pr-4 pl-4">
+                      <h3 className="text-white text-2xl font-blog cursive text-center">
+                        {post.title}
+                      </h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
+  )
 }
